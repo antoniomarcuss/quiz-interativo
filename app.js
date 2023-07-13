e84a9d6b2772047d6ced1fa86c46027d23a97454
antoniomarcuss <105389155+antoniomarcuss@users.noreply.github.com>
2023-07-13 16:45:34 -0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,77 @@
+const form = document.querySelector('form');
+const finalScoreContainer = document.querySelector('.result');
+const correctAnswers = ['A', 'C', 'D', 'B', 'A', 'D', 'C', 'B', 'A', 'A'];
+let score = 0;
+
+const userAnswers = [];
+
+const getUserAnswers = () => {
+  correctAnswers.forEach((_, index) => {
+    const userAnswer = form[`inputQuestion${index + 1}`].value;
+    userAnswers.push(userAnswer);
+  });
+};
+
+const calculateUserScore = () => {
+  userAnswers.forEach((answer, index) => {
+    const isUserAnswerCorrect = answer === correctAnswers[index];
+    if (isUserAnswerCorrect) {
+      score += 10;
+    }
+  });
+};
+
+const markCorrectAnswers = () => {
+  correctAnswers.forEach((correctAnswer, index) => {
+    const questionIndex = index + 1;
+    const correctInput = document.querySelector(`input[name="inputQuestion${questionIndex}"][value="${correctAnswer}"]`);
+    correctInput.parentElement.classList.add('success');
+  });
+};
+
+const markUserAnswers = () => {
+  userAnswers.forEach((userAnswer, index) => {
+    const questionIndex = index + 1;
+    const userInput = document.querySelector(`input[name="inputQuestion${questionIndex}"][value="${userAnswer}"]`);
+    const correctAnswer = correctAnswers[questionIndex - 1];
+    const isValidAnswer = userAnswer === correctAnswer;
+
+    if (isValidAnswer) {
+      userInput.parentElement.classList.add('success');
+    } else {
+      userInput.parentElement.classList.add('error');
+      const correctInput = document.querySelector(`input[name="inputQuestion${questionIndex}"][value="${correctAnswer}"]`);
+      correctInput.parentElement.classList.add('success');
+    }
+  });
+};
+
+const showFinalScore = () => {
+  scrollTo({
+    top: 0,
+    left: 0,
+    behavior: 'smooth'
+  });
+  finalScoreContainer.classList.remove('d-none');
+};
+
+const animationFinalScore = () => {
+  let counter = 0;
+  const time = setInterval(() => {
+    if (counter === score) {
+      clearInterval(time);
+    }
+    finalScoreContainer.querySelector('span').textContent = `${counter++}%`;
+  }, 25);
+};
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  score = 0;
+  getUserAnswers();
+  calculateUserScore();
+  showFinalScore();
+  animationFinalScore();
+  markCorrectAnswers();
+  markUserAnswers();
+})
